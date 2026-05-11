2f3e346483a4b59d039d92c512c9722e75c90ce0
Zsolt Nemesnyik M3 <nemesnyik.zsolt@gmail.com>
2026-05-11 19:35:19 +0200

    add product queries

---
+++ src/lib/queries.ts
@@ -0,0 +1,23 @@
+import { prisma } from "./prisma";
+
+export async function getProducts() {
+  return prisma.product.findMany({
+    where: { isAvailable: true },
+    include: { category: true },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
+export async function getProductBySlug(slug: string) {
+  return prisma.product.findUnique({
+    where: { slug },
+    include: { category: true },
+  });
+}
+
+export async function getFeaturedProducts() {
+  return prisma.product.findMany({
+    where: { isFeatured: true, isAvailable: true },
+    include: { category: true },
+  });
+}
