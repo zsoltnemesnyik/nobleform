f67c970b7ed45bc25994ab67358ecebcf1b0caff
Zsolt Nemesnyik M3 <nemesnyik.zsolt@gmail.com>
2026-05-14 11:51:30 +0200

    fix Prisma import error (in Vercel) - vol.4

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
