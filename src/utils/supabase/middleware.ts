f822d4dd6fc70c59e9ee3926d24af5e5d2ff0acd
Zsolt Nemesnyik M3 <nemesnyik.zsolt@gmail.com>
2026-05-18 15:59:14 +0200

    add route protection by creating middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,12 +1,13 @@
-import { createServerClient } from '@supabase/ssr';
-import { NextResponse, type NextRequest } from 'next/server';
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({ request });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, // deprecated
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll() {
@@ -14,19 +15,27 @@ export async function updateSession(request: NextRequest) {
         },
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
+            request.cookies.set(name, value),
           );
           supabaseResponse = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
+            supabaseResponse.cookies.set(name, value, options),
           );
         },
       },
-    }
+    },
   );
 
-  // FONTOS: getUser() hívása kötelező a session frissítéséhez
-  await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const isAdminRoute = request.nextUrl.pathname.startsWith("/admin");
+  const isLoginPage = request.nextUrl.pathname === "/admin/login";
+
+  if (isAdminRoute && !isLoginPage && !user) {
+    return NextResponse.redirect(new URL("/admin/login", request.url));
+  }
 
   return supabaseResponse;
 }
